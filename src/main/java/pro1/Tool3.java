9c0feeb23377e4c17e15f6838d7cf7297acbbce0
Vojta Vorel <vorelvo2@uhk.cz>
2025-03-10 12:08:46 +0100

    Po cviku 5

--- src/main/java/pro1/Tool3.java
+++ src/main/java/pro1/Tool3.java
@@ -3,18 +3,33 @@ package pro1;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.List;
 
 public class Tool3 {
     public static String analyze(Path path)
     {
         List<String> lines;
+        HashMap<String,Integer> hashMap = new HashMap<>();
         try {
             lines = Files.readAllLines(path);
-        } catch (IOException e) {
+            for(String line : lines)
+            {
+                if(hashMap.containsKey(line)) {
+                    int current = hashMap.get(line);
+                    hashMap.put(line, current+1);
+                }
+                else {
+                    hashMap.put(line,1);
+                }
+            }
+        }
+        catch (IOException e) {
             throw new RuntimeException(e);
         }
 
+        // TODO: Dodělat výpis celé hashmapy do jednoho stringu
+        //  odděleného čárkami a dvojtečkami
         return "Jasno:0,Zataženo:0";
     }
 
